b8e933bda84f4fc83cc49334091a24f6db23075c
Nankurunaisa99 <speranzabiagio99@gmail.com>
2022-11-10 11:44:39 +0100

    aggiunto una variabile per prova pt2

--- app/src/main/java/com/example/progettoingsw2022_2/MainActivity.java
+++ app/src/main/java/com/example/progettoingsw2022_2/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int ciao = 0;
+        int varieprove = 0;
         for (int i = 0; i < 10; i++) {
             Log.println(Log.INFO, "prova", "ciao");
         }
