e372350993706a8a1d216f5e29ea29009b7a986f
Luigi Vessella <vessellaluigi12@gmail.com>
2022-12-20 11:17:24 +0100

    Aggiunta tutta la parte networking

--- app/src/main/java/com/example/progettoingsw2022_2/MainActivity.java
+++ app/src/main/java/com/example/progettoingsw2022_2/MainActivity.java
@@ -8,13 +8,15 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button registerButton;
+    private Button registerButton, loginButton;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         registerButton = findViewById(R.id.registerBtn);
+        loginButton = findViewById(R.id.loginBtn);
         //Setta il onClickListener per il bottone "registrati"
-        registerButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, SignActivity.class)));
+        registerButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, ResgisterActivity.class)));
+        loginButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, SignActivity.class)));
     }
 }
--- app/src/main/java/com/example/progettoingsw2022_2/SignActivity.java
+++ app/src/main/java/com/example/progettoingsw2022_2/SignActivity.java
@@ -3,6 +3,25 @@ package com.example.progettoingsw2022_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+import NetworkManager.VolleySingleton;
 
 public class SignActivity extends AppCompatActivity {
 
@@ -10,12 +29,49 @@ public class SignActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
-        if (savedInstanceState == null)
-        {
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed(true)
-                    .add(R.id.container, RegisterFrament.class, null)
-                    .commit();
+
+        try {
+            RequestQueue queue = VolleySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
+            String url = "http://192.168.1.8:8080/admin/addNew";
+
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("VOLLEY", response);
+                }
+            },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("VOLLEY", error.toString());
+                        }
+            }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("nome", "pollo");
+                    params.put("cognome", "bello");
+                    params.put("partitaIva", "HY6680");
+
+                    return params;
+                }
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("Content-Type", "application/x-www-form-urlencoded");
+                    return params;
+                }
+            };
+            queue.add(stringRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+
+
+
     }
 }
