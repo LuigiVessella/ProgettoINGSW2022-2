49e6cb9986d00aa0753b9e7cc45a5b23b76b16a2
Nankurunaisa99 <speranzabiagio99@gmail.com>
2023-02-11 13:11:42 +0100

    Ho modificato un po' il design della mainActivity e modificato i bottoni aggiungendo i lochi e aggiustando i colori

--- app/src/main/java/com/example/progettoingsw2022_2/Activities/MainActivity.java
+++ app/src/main/java/com/example/progettoingsw2022_2/Activities/MainActivity.java
@@ -9,6 +9,7 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.progettoingsw2022_2.R;
 import com.google.android.material.appbar.MaterialToolbar;
@@ -16,6 +17,7 @@ import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class MainActivity extends AppCompatActivity {
     private Button registerButton, loginButton;
+    private TextView accediText, registraText;
     private BottomAppBar mBottomAppBar;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,6 +35,19 @@ public class MainActivity extends AppCompatActivity {
         animationDrawable.start();
         registerButton = findViewById(R.id.registerBtn);
         loginButton = findViewById(R.id.loginBtn);
+        accediText = findViewById(R.id.signedQuestion);
+        registraText = findViewById(R.id.signupQuestion);
+
+        accediText.setAlpha(0);
+        registraText.setAlpha(0);
+        loginButton.setAlpha(0);
+        registerButton.setAlpha(0);
+
+        registerButton.animate().alpha(1.0f).setDuration(4000).start();
+        loginButton.animate().alpha(1.0f).setDuration(4000).start();
+        accediText.animate().alpha(1.0f).setDuration(4000).start();
+        registraText.animate().alpha(1.0f).setDuration(4000).start();
+
         //Setta il onClickListener per il bottone "registrati"
         registerButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, ResgisterActivity.class)));
         loginButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, SignActivity.class)));
