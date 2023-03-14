638e99dfa7304675fccc7521da7b4f3369b678d1
Nankurunaisa99 <speranzabiagio99@gmail.com>
2023-03-14 16:34:22 +0100

    aggiunto tavoli

--- app/src/main/java/com/example/progettoingsw2022_2/Activities/TableStatusActivity.java
+++ app/src/main/java/com/example/progettoingsw2022_2/Activities/TableStatusActivity.java
@@ -32,9 +32,9 @@ public class TableStatusActivity extends AppCompatActivity {
     }
 
     private void setUpTables(){
-        String[] orderName = {"Ordine_1", "Ordine_2", "Ordine_3"};
-        int[] tableNumber = {10, 7, 4};
-        int[] percentage = {10, 0, 75};
+        String[] orderName = {"Ordine_1", "Ordine_2", "Ordine_3", "Ordine_4", "Ordine_5", "Ordine_6", "Ordine_7", "Ordine_8", "Ordine_9", "Ordine_10", "Ordine_11", "Ordine_12", "Ordine_13", "Ordine_14", "Ordine_15", "Ordine_16", "Ordine_17", "Ordine_18"};
+        int[] tableNumber = {10, 7, 4, 5, 3, 8, 12, 6, 9, 1, 11, 2, 14, 15, 18, 16, 13, 17};
+        int[] percentage = {10, 0, 75, 50, 20, 30, 40, 70, 90, 5, 15, 25, 35, 45, 55, 65, 80, 95};
 
         for(int i = 0; i < orderName.length; i++){
             tavoli.add(new Tavolo(orderName[i], tableNumber[i], percentage[i]));
