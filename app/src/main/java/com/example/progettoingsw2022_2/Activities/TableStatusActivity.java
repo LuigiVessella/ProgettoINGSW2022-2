f645c0589812f8aefc9b2b22e603443d7c6c529c
Luigi Vessella <vessellaluigi12@gmail.com>
2023-03-14 17:15:41 +0100

    tavolo fixed

--- app/src/main/java/com/example/progettoingsw2022_2/Activities/TableStatusActivity.java
+++ app/src/main/java/com/example/progettoingsw2022_2/Activities/TableStatusActivity.java
@@ -32,6 +32,9 @@ public class TableStatusActivity extends AppCompatActivity {
     }
 
     private void setUpTables(){
+
+        //chiamata a spring getOrdinazioni
+        //riempiamo gli array
         String[] orderName = {"Ordine_1", "Ordine_2", "Ordine_3", "Ordine_4", "Ordine_5", "Ordine_6", "Ordine_7", "Ordine_8", "Ordine_9", "Ordine_10", "Ordine_11", "Ordine_12", "Ordine_13", "Ordine_14", "Ordine_15", "Ordine_16", "Ordine_17", "Ordine_18"};
         int[] tableNumber = {10, 7, 4, 5, 3, 8, 12, 6, 9, 1, 11, 2, 14, 15, 18, 16, 13, 17};
         int[] percentage = {10, 0, 75, 50, 20, 30, 40, 70, 90, 5, 15, 25, 35, 45, 55, 65, 80, 95};
