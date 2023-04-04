a1327de730b01f69eaf4353615b164d4266cd239
Luigi Vessella <vessellaluigi12@gmail.com>
2023-04-04 16:01:07 +0200

    implementata visualizzazione menu in ordini

--- app/src/main/java/com/example/progettoingsw2022_2/Models/AddettoCucina.java
+++ app/src/main/java/com/example/progettoingsw2022_2/Models/AddettoCucina.java
@@ -3,4 +3,28 @@ package com.example.progettoingsw2022_2.Models;
 import java.io.Serializable;
 
 public class AddettoCucina extends Lavoratore {
+
+    private Ristorante ristorante;
+
+    public Ristorante getRistorante() {
+        return ristorante;
+    }
+
+    public void setRistorante(Ristorante ristorante) {
+        this.ristorante = ristorante;
+    }
+
+    public int getNumeroOrdiniEvasi() {
+        return numeroOrdiniEvasi;
+    }
+
+    public void setNumeroOrdiniEvasi(int numeroOrdiniEvasi) {
+        this.numeroOrdiniEvasi = numeroOrdiniEvasi;
+    }
+
+    private int numeroOrdiniEvasi;
+
+
+    public AddettoCucina(){}
+
 }
