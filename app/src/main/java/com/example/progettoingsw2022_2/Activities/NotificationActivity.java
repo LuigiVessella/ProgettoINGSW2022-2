56fc45f7f4210c32e37ab86aa7e4ab179a669fbd
Nankurunaisa99 <speranzabiagio99@gmail.com>
2023-04-13 18:01:35 +0200

    Avvisi

---
+++ app/src/main/java/com/example/progettoingsw2022_2/Activities/NotificationActivity.java
@@ -0,0 +1,69 @@
+package com.example.progettoingsw2022_2.Activities;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.progettoingsw2022_2.Adapter.NotificationRecycleViewAdapter;
+import com.example.progettoingsw2022_2.HttpRequest.VolleyCallback;
+import com.example.progettoingsw2022_2.Models.Avviso;
+import com.example.progettoingsw2022_2.Models.Ristorante;
+import com.example.progettoingsw2022_2.R;
+import com.example.progettoingsw2022_2.SingletonModels.AddettoCucinaSingleton;
+import com.example.progettoingsw2022_2.SingletonModels.AdminSingleton;
+import com.example.progettoingsw2022_2.SingletonModels.CameriereSingleton;
+import com.example.progettoingsw2022_2.SingletonModels.SupervisoreSingleton;
+
+import java.util.ArrayList;
+
+public class NotificationActivity extends AppCompatActivity implements VolleyCallback {
+
+    private ArrayList<Avviso> avvisi = new ArrayList<>();
+
+    private NotificationRecycleViewAdapter adapter;
+    private RecyclerView recycleView;
+    private Handler handler = new Handler();
+    private Runnable runnable;
+    private int delay = 5000;
+
+    private Ristorante ristorante;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(R.layout.activity_notifications);
+
+        if(AddettoCucinaSingleton.getInstance().getAccount() != null) ristorante = AddettoCucinaSingleton.getInstance().getAccount().getRistorante();
+        else if(CameriereSingleton.getInstance().getAccount() != null) ristorante = CameriereSingleton.getInstance().getAccount().getRistorante();
+        else if(SupervisoreSingleton.getInstance().getAccount() != null) ristorante = SupervisoreSingleton.getInstance().getAccount().getRistorante();
+
+        inizializzacomponenti();
+    }
+
+    private void inizializzacomponenti() {
+        recycleView = findViewById(R.id.activity_table_rvw);
+        recycleView.setAdapter(adapter);
+        recycleView.setLayoutManager(new LinearLayoutManager(this));
+
+        setUpNotification();
+    }
+
+    private void setUpNotification() {
+        avvisi = (ArrayList<Avviso>) ristorante.getAvvisi();
+    }
+
+    @Override
+    public void onSuccess(String result) {
+
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/progettoingsw2022_2/Adapter/NotificationRecycleViewAdapter.java
@@ -0,0 +1,76 @@
+package com.example.progettoingsw2022_2.Adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.progettoingsw2022_2.HttpRequest.CustomRequest;
+import com.example.progettoingsw2022_2.HttpRequest.VolleyCallback;
+import com.example.progettoingsw2022_2.Models.Avviso;
+import com.example.progettoingsw2022_2.Models.Lavoratore;
+import com.example.progettoingsw2022_2.Models.Ordine;
+import com.example.progettoingsw2022_2.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NotificationRecycleViewAdapter extends RecyclerView.Adapter<NotificationRecycleViewAdapter.MyViewHolder> implements VolleyCallback {
+    private final Context context;
+    private ArrayList<Avviso> avvisi;
+
+    public NotificationRecycleViewAdapter(Context context, ArrayList<Avviso> avvisi, Lavoratore dipendente) {
+        this.context = context;
+        this.avvisi = avvisi;
+
+    }
+    @NonNull
+    @Override
+    public NotificationRecycleViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view  = inflater.inflate(R.layout.notifications_row, parent, false);
+        return new NotificationRecycleViewAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotificationRecycleViewAdapter.MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
+
+        holder.notificationText.setText(String.valueOf(avvisi.get(position).getDescrizione()));
+        holder.notificationDate.setText(String.valueOf(avvisi.get(position).getDataEmissione()));
+        holder.notificationID.setText(String.valueOf(avvisi.get(position).getId_avviso()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return avvisi.size();
+    }
+
+    @Override
+    public void onSuccess(String result) {
+        System.out.println("done");
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView notificationText, notificationID, notificationDate;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            notificationDate = itemView.findViewById(R.id.notifica_data);
+            notificationID = itemView.findViewById(R.id.id_avviso);
+            notificationText = itemView.findViewById(R.id.notificationText);
+
+        }
+    }
+
+
+}
