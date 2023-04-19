0f720430fbc9bc9a99d3232e23f9b3acf2f44d43
Luigi Vessella <vessellaluigi12@gmail.com>
2023-04-19 16:34:50 +0200

    eliminazione dipendenti

--- app/src/main/java/com/example/progettoingsw2022_2/Activities/PlateManagerActivity.java
+++ app/src/main/java/com/example/progettoingsw2022_2/Activities/PlateManagerActivity.java
@@ -60,7 +60,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
